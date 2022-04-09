012c97658aa3bf39997062e89c7e921ebffb9727
Jesus <jgalanllano@gmail.com>
2022-04-09 14:03:28 +0200

    First commit

---
+++ etl.py
@@ -0,0 +1,26 @@
+﻿import requests
+
+
+def extraction():
+    '''
+    Download data from GitHub repository
+    '''
+    pass
+
+
+def transformation():
+    '''
+    Clean all data
+    '''
+
+
+def load():
+    '''
+    Load data into PostgreSQL database
+    '''
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
